93c9c10c3f3946987cdd3c72fb71ceb795ed0088
bfclouds <834298507@qq.com>
2020-03-02 12:59:12 +0800

    add header

---
+++ src/page/home/components/header.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="header">
+      <div class="header-left">
+        <span class="iconfont icon-xiazai6"></span>
+      </div>
+      <div class="header-input">
+        <span class="iconfont icon-sousuo" ></span>
+        热点城市/旅游
+      </div>
+      <div class="header-right">
+        北京<span class="iconfont icon-jiantou arrow-icon" ></span>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varbles.styl'
+  .header
+    display: flex
+    line-height: .86rem
+    background: $bgColor
+    color: #fff
+    .header-left
+      width: .64rem
+      float: left
+      text-align: center
+    .header-input
+      flex: 1
+      margin-top: .12rem
+      margin-left: .1rem
+      padding-left: .2rem
+      height: .62rem
+      line-height: .62rem
+      color: #ccc
+      background: #fff
+      border-radius: .1rem
+    .header-right
+      width: 1.24rem
+      float: right
+      text-align: center
+      .arrow-icon
+        font-size: .2rem
+</style>
---
+++ src/page/home/home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <home-header></home-header>
+  </div>
+</template>
+
+<script>
+import HomeHeader from '@/page/home/components/header.vue'
+
+export default {
+
+  components: {
+    HomeHeader
+  }
+}
+</script>
+
+<style></style>
