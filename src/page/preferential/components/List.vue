a890784dd1a4f2fe8eb79af480c098470da99b1f
bfclouds <834298507@qq.com>
2020-03-15 23:13:59 +0800

    detail cycle components

--- src/page/preferential/components/List.vue
+++ src/page/preferential/components/List.vue
@@ -9,6 +9,10 @@
         <span class="item-title-icon"></span>
         {{item.title}}
       </div>
+      <div class="item-content">{{item.content}}</div>
+      <div v-if="item.child">
+        <preferential-list :list="item.child"></preferential-list>
+      </div>
     </div>
   </div>
 </template>
@@ -25,13 +29,20 @@ export default {
 <style lang="stylus" scoped>
   .list
     .item
+      padding 0 .2rem
       .item-title
-        padding 0 .2rem
-        line-height .8rem
+        margin-top .1rem
+        margin-bottom .2rem
+        line-height .5rem
         font-size .32rem
+        border-bottom 1px solid #ccc
         .item-title-icon
           display inline-block
           width .36rem
           height .36rem
           margin-right .1rem
+      .item-content
+        padding 0 .2rem
+        font-size .28rem
+        line-height .32rem
 </style>
