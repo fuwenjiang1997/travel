1f5bc040b1490adc5224402ce2519a362b1d78e6
bfclouds <834298507@qq.com>
2020-03-16 10:38:46 +0800

    add details remind component

--- src/page/detail/Detail.vue
+++ src/page/detail/Detail.vue
@@ -2,6 +2,7 @@
   <div class="detail">
     <detail-header></detail-header>
     <detail-banner />
+    <detail-remind></detail-remind>
     <div class="content">
       123
 <!--      <detail-list :list="list"></detail-list>-->
@@ -12,12 +13,14 @@
 <script>
 import DetailBanner from './components/Banner'
 import DetailHeader from './components/Header'
+import DetailRemind from './components/remind'
 
 export default {
   name: 'Detail',
   components: {
     DetailBanner,
-    DetailHeader
+    DetailHeader,
+    DetailRemind
   }
 }
 </script>
---
+++ src/page/detail/components/remind.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="remind">
+    <span class="iconfont icon-tixing remind-icon-style" ></span>
+    <p>{{desc}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'remind',
+  data () {
+    return {
+      desc: '自1月31日起，暂停给两周在中国生活的外国人签发旅游签证；暂停中越间往返航班；暂停办理经东兴-芒街口岸出入境的人员手续。'
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .remind
+    overflow auto
+    padding .2rem .22rem
+    color #f4660a
+    background #fefaeb
+    .remind-icon-style
+      display inline-block
+      float left
+      margin-right .08rem
+    p
+      line-height .38rem
+</style>
